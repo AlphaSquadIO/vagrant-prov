be2f1afb200bb0940f18a820e28a92f803461041
Peter Wong <peter.wong2@fujixerox.com>
2018-11-07 10:16:20 +1100

    More changes

--- lib/bootstrap-devtools.sh
+++ lib/bootstrap-devtools.sh
@@ -7,5 +7,5 @@ BOOTSTRAPPER_HOME=/home/vagrant/vagrant-prov
 
 ansible-playbook -i $BOOTSTRAPPER_HOME/config/devtools/hosts $BOOTSTRAPPER_HOME/config/devtools/devtools-dependencies.yml
 
-ansible-galaxy install -r $BOOTSTRAPPER_HOME/config/devtools/requirements.yml
+#ansible-galaxy install -r $BOOTSTRAPPER_HOME/config/devtools/requirements.yml
 ansible-playbook -i $BOOTSTRAPPER_HOME/config/devtools/hosts $BOOTSTRAPPER_HOME/config/devtools/devtools.yml
